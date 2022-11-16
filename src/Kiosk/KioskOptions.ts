a23a5bb01a2351c3ef6d61fdaf9e108a03ffff50
Ngras <nicolas.g33@hotmail.fr>
2022-11-16 16:47:58 +0100

    wip

--- src/Kiosk/KioskOptions.ts
+++ src/Kiosk/KioskOptions.ts
@@ -17,11 +17,38 @@ export default class KioskOptionsModule extends SlideOptionsModule {
 
     const update = context.update;
 
-    const { Field, TextInput, Toggle } = this.context.components
+    const { Field, TextInput, Toggle, MediaPicker, NumberInput, FieldsRow } = this.context.components
 
-    return () =>
-      h("div", {}, [
-      ]
-    )
+    return () => [
+        h(Field, { label: this.t('modules.kiosk.options.new_document_date') }, [
+            h("div", {
+                class: "flex flex-row items-center space-x-2"
+            }, [
+                h(NumberInput, {
+                    min: 1, max: 100, default: 5, ...update.option("notification_duration"),
+                    class: "w-1/4"
+                }),
+                h("span", {
+                    class: "text-sm"
+                }, this.t("modules.kiosk.options.days"))
+            ]),
+            h("span", {
+                class: "text-xs text-gray-400"
+            }, this.t("modules.kiosk.options.new_document_date_desc"))
+        ]),
+        h(Field, { label: this.t('modules.kiosk.options.notification_time') }, [
+            h("div", {
+                class: "flex flex-row items-center space-x-2"
+            }, [
+                h(TextInput, {
+
+                    class: "w-1/4"
+                }),
+            ]),
+            h("span", {
+                class: "text-xs text-gray-400"
+            }, this.t("modules.kiosk.options.new_document_date_desc"))
+        ]),
+    ];
   }
 }
