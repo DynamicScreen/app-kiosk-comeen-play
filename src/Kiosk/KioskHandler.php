9f6607f3d3c535f71c5791d556b83599a6ef87cf
Ngras <nicolas.g33@hotmail.fr>
2022-11-23 09:34:55 +0100

    add medias privilege inside manifest + Retrieve data to display inside the Handler

--- src/Kiosk/KioskHandler.php
+++ src/Kiosk/KioskHandler.php
@@ -5,13 +5,40 @@ namespace Comeen\Kiosk\Kiosk;
 use ComeenPlay\SdkPhp\Handlers\SlideHandler;
 use ComeenPlay\SdkPhp\Interfaces\ISlide;
 use ComeenPlay\SdkPhp\Interfaces\IDisplay;
+use ComeenPlay\SdkPhp\Modules\LibraryModule;
+use Illuminate\Support\Arr;
 
 class KioskHandler extends SlideHandler
 {
     public function fetch(ISlide $slide, IDisplay $display): void
     {
+        $list = LibraryModule::listFolders($display);
+        $categoriesOptions = $slide->getOption("categories", []);
+
+        foreach ($categoriesOptions as $key => $category) {
+            if ($category["type"] === "folders") {
+                $categoryFolders = $category["folders"];
+                $medias = [];
+                foreach ($categoryFolders as $idFolder) {
+                    $listFoldersId = array_column($list->folders, "id");
+                    $folderId = array_search((int) $idFolder, $listFoldersId);
+                    array_push($medias, $list->folders[$folderId]);
+                }
+                $categoriesOptions[$key]["folders"] = $medias;
+            }
+        }
+
+//        $mediasAccessKey = $this->needed_medias();
+//
+//        if (is_array($mediasAccessKey)) {
+//            $mediasAccessKey = Arr::first($mediasAccessKey);
+//        }
+//
+//        dd($slide->getMedias());
+//        $bg_media = $slide->getMedias($mediasAccessKey);
+
         $this->addSlide([
-            // 'url' => $slide->getOption('url', ""),
+            'categories' => $categoriesOptions,
         ]);
     }
 
